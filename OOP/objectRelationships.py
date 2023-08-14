91a288a85d62b1d57422ac7fa854b4627c96fa23
Obasi <objtorti@hotmail.com>
2023-08-14 19:19:26 +0100

    Update objectRelationships.py

--- OOP/objectRelationships.py
+++ OOP/objectRelationships.py
@@ -68,3 +68,93 @@
 # car1.printDetails()
 # car1.setStop()
 
+# class Player:
+#     def __init__(self, ID, name, teamName):
+#         self.id = ID
+#         self.name = name
+#         self.teamname = teamName
+
+# class Team:
+#     def __init__(self, name):
+#         self.name = name
+#         self.players = []
+
+#     def addPlayer(self, newPlayer):
+#         newPlayer =[]
+#         self.newPlayer = newPlayer.append(self.players)
+
+#     def getNumberOfPlayers(self):
+#         return (len(self.newPlayer))
+    
+
+# class School:
+#     def __init__(self, teams, name):
+#         teams = []
+#         self.teams = teams.append(Team)
+#         self.name = name
+
+#     def addTeam(self, newTeam):
+#         newTeam = []
+#         self.newTeam = newTeam.append(self.teams)
+    
+#     def getTotalPlayersInSchool(self):
+#         return (len(self.newTeam))
+
+# Player1 = Player("012", "Bola", "Tigers")
+# Player2 = Player ("013", "Chika", "Tigers")
+# Team1 = Team("Tigers", "2")
+# Team1.addPlayer("James") 
+# print(Team1.getNumberOfPlayers())
+
+class Player:
+    def __init__(self, ID, name, teamName):
+        self.ID = ID
+        self.name = name
+        self.teamName = teamName
+
+
+class Team:
+    def __init__(self, name):
+        self.name = name
+        self.players = []
+
+    def getNumberOfPlayers(self):
+        return len(self.players)
+
+    def addPlayer(self, player):
+        self.players.append(player)
+
+
+class School:
+    def __init__(self, name):
+        self.name = name
+        self.teams = []
+
+    def addTeam(self, team):
+        self.teams.append(team)
+
+    def getTotalPlayersInSchool(self):
+        length = 0
+        for n in self.teams:
+            length = length + (n.getNumberOfPlayers())
+        return length
+
+
+p1 = Player(1, "Harris", "Red")
+p2 = Player(2, "Carol", "Red")
+p3 = Player(1, "Johnny", "Blue")
+p4 = Player(2, "Sarah", "Blue")
+
+red_team = Team("Red Team")
+red_team.addPlayer(p1)
+red_team.addPlayer(p2)
+
+blue_team = Team("Blue Team")
+blue_team.addPlayer(p2)
+blue_team.addPlayer(p3)
+
+mySchool = School("My School")
+mySchool.addTeam(red_team)
+mySchool.addTeam(blue_team)
+
+print("Total players in mySchool:", mySchool.getTotalPlayersInSchool())
