b2efe43c6c2ef0c80818c556435cff6676309748
Obasi <objtorti@hotmail.com>
2023-08-21 18:03:52 +0100

    Update singlyLinked.py

---
+++ DataStructuresAndAlgorithms/linkedLists/singlyLinked.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class LinkedList:
+    def __init__(self):
+        self.head = None
+    
+    def append(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+            return
