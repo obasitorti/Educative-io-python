568cae1cce8cfa4229442c66be1708a0f7b7136b
Obasi <104761686+obasitorti@users.noreply.github.com>
2023-09-18 11:44:03 +0100

    Update circularlinked.py

--- DataStructuresAndAlgorithms/linkedLists/circularlinked.py
+++ DataStructuresAndAlgorithms/linkedLists/circularlinked.py
@@ -45,4 +45,35 @@ class CircularLinkedList:
             cur.next = new_node
         self.head = new_node
 
-    
+    def remove(self, key):
+        if self.head:
+            if self.head.data == key:
+                cur = self.head
+                while cur.next != self.head:
+                    cur = cur.next
+                if self.head == self.head.next:
+                    self.head = None
+                else:
+                    cur.next = self.head.next
+                    self.head = self.head.next
+            else:
+                cur = self.head
+                prev = None
+                while cur.next != self.head:
+                    prev = cur
+                    cur = cur.next
+                    if cur.data == key:
+                        prev.next = cur.next
+                        cur = cur.next
+
+
+circularlink = CircularLinkedList()
+circularlink.append("C")
+circularlink.append("E")
+circularlink.prepend("2")
+circularlink.prepend("1")
+circularlink.print_list()
+print()
+circularlink.remove("C")
+circularlink.remove("1")
+circularlink.print_list()
