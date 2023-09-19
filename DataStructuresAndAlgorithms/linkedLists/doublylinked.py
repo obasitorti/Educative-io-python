bdc3da11069f0600ff28c49d028404f41b46e1f9
Obasi <104761686+obasitorti@users.noreply.github.com>
2023-09-19 19:49:46 +0100

    Update doublylinked.py

--- DataStructuresAndAlgorithms/linkedLists/doublylinked.py
+++ DataStructuresAndAlgorithms/linkedLists/doublylinked.py
@@ -69,16 +69,84 @@ class DoublyLinkedList:
                 return 
             cur = cur.next
 
+    def delete(self, key):
+        cur = self.head
+        while cur:
+            if cur.data == key and cur == self.head:
+                if not cur.next:
+                    cur = None
+                    self.head = None
+                    return
+                else:
+                    nxt = cur.next
+                    cur.next = None
+                    nxt.prev = None
+                    cur = None
+                    self.head = nxt
+                    return
+            
+            elif cur.data == key:
+                if cur.next:
+                    nxt = cur.next
+                    prev = cur.prev
+                    prev.next = nxt
+                    nxt.prev = prev
+                    cur.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+                
+                else:
+                    prev = cur.prev
+                    prev.next = None
+                    cur.prev = None
+                    cur = None
+                    return
+            cur = cur.next 
+    
+    def reverse(self):
+        tmp = None
+        cur = self.head
+        while cur:
+            tmp = cur.prev
+            cur.prev = cur.next
+            cur.next = tmp
+            cur = cur.prev
+        if tmp:
+            self.head = tmp.prev
+
+    def remove_duplicates(self):
+        cur = self.head
+        while cur:
+            tmp = cur.next
+            while tmp:
+                if tmp.data == cur.data:
+                    self.delete(tmp.data)
+                tmp = tmp.next
+            cur = cur.next
+    
+    def pairs_with_sum(self, sum_val):
+        cur = self.head
+        list1 = []
+        while cur:
+            tmp = cur.next
+            while tmp:
+                if cur.data + tmp.data == sum_val:
+                    vals1 = "({0}, {1})".format(cur.data, tmp.data)
+                    list1.append (vals1)
+                tmp = tmp.next
+            
+            cur = cur.next
+        return(list1)
+
+
 
 dllist = DoublyLinkedList()
-
-dllist.prepend(0)
 dllist.append(1)
 dllist.append(2)
 dllist.append(3)
 dllist.append(4)
-dllist.prepend(5)
-dllist.add_after_node(3,6)
-dllist.add_before_node(4,9)
-
+dllist.pairs_with_sum(7)
+# dllist.append(4)
+# dllist.remove_duplicates()
 dllist.print_list()
