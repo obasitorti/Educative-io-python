45a8cf3374ec25aea80f04cce05ece1c28e7c8e0
Obasi <104761686+obasitorti@users.noreply.github.com>
2023-09-22 15:07:24 +0100

    Update bisect_method.py

--- DataStructuresAndAlgorithms/binary search/bisect_method.py
+++ DataStructuresAndAlgorithms/binary search/bisect_method.py
@@ -3,3 +3,15 @@ import bisect
 A = [-14, -10, 2, 108, 108, 243, 285, 285, 285, 401]
 
 print(bisect.bisect_left(A, -10))
+
+print(bisect.bisect_right(A, -10))
+
+print(bisect.bisect_right(A, 285))
+
+print(bisect.bisect(A, 285))
+
+print(A)
+bisect.insort_left(A, 108)
+print(A)
+bisect.insort_right(A, 108)
+print(A)
