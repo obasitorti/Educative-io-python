639cabdddeb7348c43c81910b92d171fa0cfc7b5
Obasi <104761686+obasitorti@users.noreply.github.com>
2023-09-29 16:42:21 +0100

    Create importing.py

---
+++ Python Advanced Concepts/Imports/importing.py
@@ -0,0 +1,6 @@
+# the importlib package gives the programmer the ability
+# to create their own custom objects (AKA an importer)
+# that can be used in the process
+
+def main():
+    print(__name__)
